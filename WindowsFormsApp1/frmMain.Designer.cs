6b5b6bd9159acca113dba3d1dcbe6e7913d24749
Bachoo <cosmin_cb@hotmail.com>
2019-12-05 21:54:38 -0500

    added files

---
+++ WindowsFormsApp1/frmMain.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnSetInput = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(168, 20);
+            this.txtInput.TabIndex = 5;
+            // 
+            // btnSetInput
+            // 
+            this.btnSetInput.Location = new System.Drawing.Point(186, 10);
+            this.btnSetInput.Name = "btnSetInput";
+            this.btnSetInput.Size = new System.Drawing.Size(75, 23);
+            this.btnSetInput.TabIndex = 1;
+            this.btnSetInput.Text = "Apply";
+            this.btnSetInput.UseVisualStyleBackColor = true;
+            this.btnSetInput.Click += new System.EventHandler(this.btnSetInput_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(22, 182);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(96, 71);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinus.Location = new System.Drawing.Point(167, 182);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(94, 71);
+            this.btnMinus.TabIndex = 5;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(105, 276);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCounter.BackColor = System.Drawing.Color.Olive;
+            this.lblCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCounter.ForeColor = System.Drawing.Color.White;
+            this.lblCounter.Location = new System.Drawing.Point(12, 36);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(249, 108);
+            this.lblCounter.TabIndex = 7;
+            this.lblCounter.Text = "0";
+            this.lblCounter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "coded by Bachoo#0001";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 26);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Pressing Z will increase the Counter. \r\nPressing X will decrease the Counter.";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 311);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSetInput);
+            this.Controls.Add(this.txtInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Shiny Hunting Tool";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnSetInput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblCounter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/frmMain.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        //important variables
+        string path = "./counter.txt";
+        int currentCount = 0;
+
+        private void writeFile()
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.Write(currentCount);
+                sw.Close();
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+
+            if (File.Exists(path))
+            {
+                //open the file and grab the counter and then close the scanner stream
+                StreamReader sr = new StreamReader(path);
+                string raw = sr.ReadLine();
+                currentCount = int.Parse(raw);
+                lblCounter.Text = raw;
+                sr.Close();
+            } else
+            {
+                writeFile();
+            }
+        }
+
+        //set, add, subtract, reset buttons while writing to file each time 
+        private void btnSetInput_Click(object sender, EventArgs e)
+        {
+            int value = int.Parse(txtInput.Text);
+            lblCounter.Text = value.ToString();
+            currentCount = value;
+            writeFile();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            currentCount += 1;
+            lblCounter.Text = currentCount.ToString();
+            writeFile();
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            currentCount -= 1;
+            lblCounter.Text = currentCount.ToString();
+            writeFile();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            currentCount = 0;
+            lblCounter.Text = "0";
+            writeFile();
+        }
+        
+        //grab all keypresses on the file while the program is in focus
+        private void frmMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Z )
+            {
+                currentCount += 1;
+                lblCounter.Text = currentCount.ToString();
+                writeFile();
+            } else if(e.KeyCode == Keys.X)
+            {
+                currentCount -= 1;
+                lblCounter.Text = currentCount.ToString();
+                writeFile();
+            }
+        }
+    }
+}
